dbdf479294a8bb3fce54273f813a92b4d7a08a7f
chrireed <129335397+chrireed@users.noreply.github.com>
2024-04-17 21:01:33 -0400

    added sections for generating individual csv files, including a raw data subset

--- generate_data.m
+++ generate_data.m
@@ -16,32 +16,36 @@ clipLength = 44100 .* secondsPerClip;
 
 %% Combine Tables
 dataTable = vertcat(songTable, adTable);
-% indices = 1:50;
-% songTable = songTable(indices, :);
-% adTable = adTable(indices, :);
-% dataTableSmall = vertcat(songTable, adTable);
 
 %% Get MFCC's
 mfccTable = getMFCC(dataTable, clipLength);
 
-%% Write data
-% labels = dataTableSmall{:, 1};
-% labels = table(labels);
-% writetable(labels,'labels.csv','Delimiter',',');
-% dataTableSmall.label = [];
-% dataTableSmall.title = [];
-% dataTableSmall.sampleRate = [];
-% writetable(dataTableSmall,'raw_data.csv','Delimiter',',');
+%% Write Labels
 labels = dataTable{:, 1};
 labels = table(labels);
 writetable(labels,'labels.csv','Delimiter',',');
-dataTable.label = [];
-dataTable.title = [];
-dataTable.sampleRate = [];
-writetable(dataTable,'raw_data.csv','Delimiter',',');
+
+%% Write MFCC data
 mfccTable.label      = [];
 mfccTable.sampleRate = [];
 writetable(mfccTable,'mfcc_data.csv','Delimiter',',');
 
+%% Write Raw Data
+dataTable.label = [];
+dataTable.title = [];
+dataTable.sampleRate = [];
+writetable(dataTable,'raw_data.csv','Delimiter',',');
 
+%% Write Raw Data Small
+indices = 1:50;
+songTable_small = songTable(indices, :);
+adTable_small = adTable(indices, :);
+dataTableSmall = vertcat(songTable_small, adTable_small);
+labelsSmall = dataTableSmall{:, 1};
+labelsSmall = table(labelsSmall);
+writetable(labelsSmall,'labels_small.csv','Delimiter',',');
 
+dataTableSmall.label = [];
+dataTableSmall.title = [];
+dataTableSmall.sampleRate = [];
+writetable(dataTableSmall,'raw_data_small.csv','Delimiter',',');
